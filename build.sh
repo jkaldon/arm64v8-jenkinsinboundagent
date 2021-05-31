a33cbb2752c480eb6365e6a49e0fcab76af8b81c
Joshua Kaldon <jkaldon@lenistech.com>
2021-05-31 00:42:22 -0500

    initial cut of arm64v8-jenkinsinboundagent

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -x
+
+docker run --rm --privileged linuxkit/binfmt:v0.8
+docker buildx build --progress plain --no-cache --pull --platform linux/arm64 -t jkaldon/arm64v8-jenkinsinboundagent:jdk11-alpine3.13 --push .
